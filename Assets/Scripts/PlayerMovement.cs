74f8e50f4e557e529c08fffd932685821a8e103b
Joel47521 <a47521@alunos.ipb.pt>
2022-05-05 17:23:05 +0100

    2-16

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float turnSpeed;
+    public float turnSpeed =20f;
     Animator m_Animator;
     Rigidbody m_Rigidbody;
     Vector3 m_Movement;
+    Quaternion m_Rotation = Quaternion.identity;
 
     // Start is called before the first frame update
     void Start()
@@ -31,9 +32,12 @@ public class PlayerMovement : MonoBehaviour
         m_Animator.SetBool("IsWalking", IsWalking);
 
         Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
+        m_Rotation = Quaternion.LookRotation(desiredForward);
     }
     void OnAnimatorMove()
     {
         m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * m_Animator.deltaPosition.magnitude);
+        m_Rigidbody.MoveRotation(m_Rotation);
+
     }
 }
